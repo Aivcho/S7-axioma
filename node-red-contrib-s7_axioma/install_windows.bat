e003201255d62bc0312fe2c0f9b9e82cff00fde9
aldisadom <aldisadom@gmail.com>
2022-03-02 17:18:03 +0200

    Update install_windows.bat

--- node-red-contrib-s7_axioma/install_windows.bat
+++ node-red-contrib-s7_axioma/install_windows.bat
@@ -1,1 +1,3 @@
+mkdir %userprofile%\.node-red\node_modules\node-red-contrib-s7_axioma
+xcopy ..\node-red-contrib-s7_axioma %userprofile%\.node-red\node_modules\node-red-contrib-s7_axioma /Y /E
 call npm install %userprofile%\.node-red\node_modules\node-red-contrib-s7_axioma
