58b1591ec36f8b01966b56f38e986a69d80a425a
aldisadom <aldisadom@gmail.com>
2022-03-03 18:40:26 +0200

    Update install_linux.sh

--- node-red-contrib-s7_axioma/install_linux.sh
+++ node-red-contrib-s7_axioma/install_linux.sh
@@ -5,4 +5,5 @@ echo "Instaling axioma node red s7 library"
 
 cd -
 cp -a node-red-contrib-s7_axioma/.  /var/lib/node-red/.node-red/node-red-contrib-s7_axioma
-npm install /var/lib/node-red/.node-red/node-red-contrib-s7_axioma
+cd /var/lib/node-red/.node-red/
+npm install node-red-contrib-s7_axioma/
